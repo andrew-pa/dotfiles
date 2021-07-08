1a78e213fbafbc4b755c3ae2c43bc99b6e95495c
andrew-pa <andrew.pa@outlook.com>
2021-07-08 06:15:25 -0700

    update make-links.sh to ignore uncolored configs and run unipalette before creating links

--- make-links.sh
+++ make-links.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-CONFIG_ROOT=$HOME/config/home
+COLORSCHEME=summer.palette
+CONFIG_ROOT=$HOME/config
 
-find $CONFIG_ROOT -type f | sed "s|$CONFIG_ROOT\(.*\)|$CONFIG_ROOT\1 $HOME\1|g" | xargs -n 2 -- ln -svf
+unipalette $CONFIG_ROOT/palettes/$COLORSCHEME expand $CONFIG_ROOT
+
+find $CONFIG_ROOT/home -type f -not -name "*.uncol" | sed "s|$CONFIG_ROOT/home\(.*\)|$CONFIG_ROOT/home\1 $HOME\1|g" | xargs -n 2 -- ln -svf
