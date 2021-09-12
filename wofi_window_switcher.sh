220d5954045bbdd9a53dd1be0e74f6eee8593a1c
andrew-pa <andrew.pa@outlook.com>
2021-09-12 11:46:42 -0600

    switch from wofi to sway-launcher-desktop

--- wofi_window_switcher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name)' | wofi --dmenu -i | { read -r id name; swaymsg "[con_id=$id]" focus; }
