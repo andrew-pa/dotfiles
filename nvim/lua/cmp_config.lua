8e7c99e67e930516e0e3d7eb417c8b0e64fccbef
andrew-pa <andrew.pa@outlook.com>
2022-06-09 07:30:54 -0700

    super tab for cmp+luasnip

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -1,6 +1,8 @@
 
 return function()
     local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
     cmp.setup {
         sources = {
             { name = "nvim_lsp" },
@@ -15,16 +17,36 @@ return function()
         },
 
         mapping = {
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-            ['<Tab>'] = cmp.mapping.select_next_item(),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+
             ['<C-d>'] = cmp.mapping.scroll_docs(-8),
             ['<C-f>'] = cmp.mapping.scroll_docs(8),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm({
-               behavior = cmp.ConfirmBehavior.Replace,
-               select = false
-           })
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = false
+            })
         },
 
         completion = {
