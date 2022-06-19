75e0a9d6c4e4e4b3ce77a64e4a3a6c189b4ac6f4
andrew-pa <andrew.pa@outlook.com>
2022-06-18 17:04:48 -0700

    <C-j,k> to scroll telescope, simplify Telescope leader bindings

--- nvim/lua/telescope_config.lua
+++ nvim/lua/telescope_config.lua
@@ -13,7 +13,9 @@ M.config = function()
         defaults = {
             mappings = {
                 i = {
-                    [ "<esc>" ] = actions.close
+                    [ "<esc>" ] = actions.close,
+                    [ "<C-j>" ] = actions.move_selection_better,
+                    [ "<C-k>" ] = actions.move_selection_worse,
                 }
             }
         }
@@ -23,9 +25,9 @@ M.config = function()
     require('telescope').load_extension('fzf')
 
     local opts = { noremap = true, silent = true }
-    vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope_config").project_files()<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua require("telescope_config").project_files()<cr>', opts)
     vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
-    vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>Telescope live_grep<cr>', opts)
     vim.api.nvim_set_keymap('n', '<leader>S', '<cmd>Telescope treesitter<cr>', opts)
     vim.api.nvim_set_keymap('n', '<leader>D', '<cmd>Telescope diagnostics<cr>', opts)
 end
